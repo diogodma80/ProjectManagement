95d79a1d2469630081cc17b76a1cb204737d3469
Diogo Almeida <diogodma80@gmail.com>
2020-11-21 13:47:31 -0500

    - Set up H2 properties in application.properties file   -   - set spring.datasource.url=jdbc:h2:mem:testdb in the properties file  - Create CRUD Repository for Project entity

--- project-management/src/main/java/com/dma/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/dma/pma/controllers/ProjectController.java
@@ -1,16 +1,21 @@
 package com.dma.pma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dma.pma.dao.ProjectRepository;
 import com.dma.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+	
+	@Autowired
+	ProjectRepository projectRepository;
 
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
@@ -22,7 +27,10 @@ public class ProjectController {
 	
 	@PostMapping("/save")
 	public String createProject(Project project, Model model) { 
-		//TODO - this method should handle saving to the database
-		return ""; 
+		// this method should handle saving to the database
+		projectRepository.save(project);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/projects/new"; 
 	}
 }
---
+++ project-management/src/main/java/com/dma/pma/dao/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.dma.pma.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dma.pma.entities.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+}
