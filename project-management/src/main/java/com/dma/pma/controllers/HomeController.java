360da94191527c39d6b54a322e8c1dfe4aea64c1
Diogo Almeida <diogodma80@gmail.com>
2020-11-21 15:58:07 -0500

    - Create the HomeController and home page display the list of projects and employees  - Override the findAll method in the ProjectRepository and EmployeeRepository so that it returns List<Object> instead of a List<Iterable>

---
+++ project-management/src/main/java/com/dma/pma/controllers/HomeController.java
@@ -0,0 +1,37 @@
+package com.dma.pma.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dma.pma.dao.EmployeeRepository;
+import com.dma.pma.dao.ProjectRepository;
+import com.dma.pma.entities.Employee;
+import com.dma.pma.entities.Project;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@GetMapping("/")
+	public String displayHome(Model model) {
+		
+		// findAll is overriden in the ProjectRepository so that a list 
+		// of Projects is returned instead of Iterable
+		List<Project> projects =  projectRepository.findAll();
+		List<Employee> employees = employeeRepository.findAll();
+		
+		model.addAttribute("projects", projects);
+		model.addAttribute("employees", employees);
+		
+		return "home";
+	}
+}
--- project-management/src/main/java/com/dma/pma/dao/EmployeeRepository.java
+++ project-management/src/main/java/com/dma/pma/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.dma.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.dma.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
+	@Override
+	List<Employee> findAll();
 }
