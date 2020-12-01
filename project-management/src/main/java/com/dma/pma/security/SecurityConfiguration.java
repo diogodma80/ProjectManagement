57d56d2eba8a726147fdeddffad0d04c3265c806
Diogo Almeida <diogodma80@gmail.com>
2020-11-30 20:13:13 -0500

    - Spring security: In-memory authorization   - Override configure method and use the HttpSecurity to match the endpoints with the roles   - Test 'ADMIN' profile to restrict others roles from creating new employees and projects

--- project-management/src/main/java/com/dma/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/dma/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.dma.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,13 +13,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	// Authentication
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.inMemoryAuthentication()
 			.withUser("myUser").password("pass").roles("USER")
 			.and()
-			.withUser("taz").password("pass2").roles("USER");
+			.withUser("taz").password("pass2").roles("USER")
+			.and()
+			.withUser("managerUser").password("pass3").roles("ADMIN");
+	}
+	
+	// Authorization
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers("/projects/new").hasRole("ADMIN")
+		.antMatchers("/employees/new").hasRole("ADMIN")
+		.antMatchers("/").authenticated()
+		.and()
+		.formLogin();
 	}
 
 	@Bean
