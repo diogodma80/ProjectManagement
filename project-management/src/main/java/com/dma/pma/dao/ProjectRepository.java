89e9147d01aca386fb0ebe14ff21215d078f1789
Diogo Almeida <diogodma80@gmail.com>
2020-12-04 10:12:31 -0500

    update

--- project-management/src/main/java/com/dma/pma/dao/ProjectRepository.java
+++ project-management/src/main/java/com/dma/pma/dao/ProjectRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dma.pma.dto.ChartData;
 import com.dma.pma.entities.Project;
 
+//Rest controller will serve from the 'apiemployees' endpoint while 'employees' will continue to serve the page
+@RepositoryRestResource(path = "apiprojects", collectionResourceRel = "apiprojects")
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long>{
 
 //	@Override
--- project-management/src/main/java/com/dma/pma/dao/UserAccountRepository.java
+++ project-management/src/main/java/com/dma/pma/dao/UserAccountRepository.java
@@ -1,9 +1,11 @@
 package com.dma.pma.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dma.pma.entities.UserAccount;
-
+//Rest controller will serve from the 'apiemployees' endpoint while 'employees' will continue to serve the page
+@RepositoryRestResource(path = "apiuseraccounts", collectionResourceRel = "apiuseraccounts")
 public interface UserAccountRepository extends PagingAndSortingRepository<UserAccount, Long> {
 
 }
