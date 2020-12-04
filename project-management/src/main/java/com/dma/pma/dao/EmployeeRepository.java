d1b321ad992c1b4d1b87afbaab7fc0983653cd2c
Diogo Almeida <diogodma80@gmail.com>
2020-12-04 10:00:37 -0500

    update

--- project-management/src/main/java/com/dma/pma/dao/EmployeeRepository.java
+++ project-management/src/main/java/com/dma/pma/dao/EmployeeRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dma.pma.dto.EmployeeProject;
 import com.dma.pma.entities.Employee;
 
+// Rest controller will serve from the 'apiemployees' endpoint while 'employees' will continue to serve the page
+@RepositoryRestResource(path = "apiemployees", collectionResourceRel = "apiemployees")
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 //	@Override
